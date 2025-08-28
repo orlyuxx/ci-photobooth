a0e7d62f94dcd00f091bd79bac54ec8911d2a228
Orlando Caesar Donesa <orlandodonesa77@gmail.com>
2025-08-28 15:49:26 +0800

    added croumatic.ico file

--- app/layout.js
+++ app/layout.js
@@ -55,6 +55,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/images/croumatic.ico" type="image/x-icon" />
         <link rel="icon" href="/images/croumatic.png" type="image/png" />
       </head>
       <body
